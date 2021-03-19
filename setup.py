0292d33efb530b66784166ee6e3ff64bf5a0f26c
Filip Dimitrovski <filipdimitrovski22@gmail.com>
2021-03-19 20:10:33 +0100

    Refactor into a package

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="nlbcli", # Replace with your own username
+    version="0.0.1",
+    author="Andrej Trajchevski",
+    author_email="andrejtrajchevski@gmail.com",
+    description="A CLI for nlbklik.com.mk -- check your balance, list transactions and such.",
+    url="https://github.com/whoeverest/nlbcli/",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    entry_points = {
+        'console_scripts': ['nlbcli=nlbcli:main']
+    },
+    install_requires=[
+        'requests>=2.25.0',
+        'beautifulsoup4>=4.9.3'
+    ],
+    package_data={
+        "nlbcli": ["*.pem"]
+    }
+)
